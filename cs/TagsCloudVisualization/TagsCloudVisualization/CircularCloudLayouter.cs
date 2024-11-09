b1b7639e2b0ae1272e0ff95e4dea19e849a73731
Aleksandr Sazonov <alexxsaz96@gmail.com>
2024-11-09 23:07:21 +0500

    Added property - list of rectangles

--- cs/TagsCloudVisualization/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -4,18 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TagsCloudVisualization
-{
-    public class CircularCloudLayouter
-    {
-        public CircularCloudLayouter(Point center)
-        {
-            
-        }
+namespace TagsCloudVisualization;
 
-        public Rectangle PutNextRectangle(Size size)
-        {
-            throw new NotImplementedException();
-        }
+public class CircularCloudLayouter
+{
+    public LinkedList<Rectangle> Rectangles { get; }
+
+    public CircularCloudLayouter(Point center)
+    {
+        Rectangles = new LinkedList<Rectangle>();
+    }
+
+    public Rectangle PutNextRectangle(Size size)
+    {
+        var newRectangle = new Rectangle(size);
+        Rectangles.AddLast(newRectangle);
+        return Rectangles.Last?.Value ?? newRectangle;
     }
 }
+
