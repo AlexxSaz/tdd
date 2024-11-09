1caa4b88b9de0dd70213dfff85244a23f2243d96
Aleksandr Sazonov <alexxsaz96@gmail.com>
2024-11-09 23:05:56 +0500

    Added setup and first tests

--- cs/TagsCloudVisualization/TagsCloudVisualization/CircularCloudLayouterShould.cs
+++ cs/TagsCloudVisualization/TagsCloudVisualization/CircularCloudLayouterShould.cs
@@ -6,6 +6,28 @@ namespace TagsCloudVisualization;
 [TestFixture]
 public class CircularCloudLayouterShould
 {
-    
+    private Point _defaultCenter;
+    private CircularCloudLayouter _defaultCircularCloudLayouter;
+    [SetUp]
+    public void SetUp()
+    {
+        _defaultCenter = new Point(0, 0);
+        _defaultCircularCloudLayouter = new CircularCloudLayouter(_defaultCenter);
+    }
+
+    [Test]
+    public void HaveZeroRectangles_AfterCreation()
+    {
+        _defaultCircularCloudLayouter.Rectangles.Should().HaveCount(0);
+    }
+
+    [TestCase(1, 1)]
+    public void PutNextRectangle_ReturnAddedRectangle_AfterExecutionWith(int width, int height)
+    {
+        var size = new Size(width, height);
+        var expectedRectangle = new Rectangle(size);
+
+        _defaultCircularCloudLayouter.PutNextRectangle(size).Should().BeEquivalentTo(expectedRectangle);
+    }
 }
 
