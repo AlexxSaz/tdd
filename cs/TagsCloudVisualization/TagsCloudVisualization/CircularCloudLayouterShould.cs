b715f1fffedde27d600f53d24330540c0c154010
Aleksandr Sazonov <alexxsaz96@gmail.com>
2024-11-10 23:40:58 +0500

    Added test for Intersection rectangles

--- cs/TagsCloudVisualization/TagsCloudVisualization/CircularCloudLayouterShould.cs
+++ cs/TagsCloudVisualization/TagsCloudVisualization/CircularCloudLayouterShould.cs
@@ -25,7 +25,6 @@ public class CircularCloudLayouterShould
     public void PutNextRectangle_ReturnAddedRectangle_AfterExecutionWith(int width, int height)
     {
         var size = new Size(width, height);
-
         var expectedRectangle = new Rectangle(size, _defaultCenter);
 
         _defaultCircularCloudLayouter.PutNextRectangle(size).Should().BeEquivalentTo(expectedRectangle);
@@ -36,11 +35,26 @@ public class CircularCloudLayouterShould
     {
         var point = new Point(x, y);
         var size = new Size(5, 2);
-
         var circularCloudLayouter = new CircularCloudLayouter(point);
         var expectedCenter = new Point(x, y);
 
         circularCloudLayouter.PutNextRectangle(size).Center.Should().BeEquivalentTo(expectedCenter);
+    }
+
+    [Test]
+    public void PutNextRectangle_ReturnRectangleThatNotCrossingWithOther_AfterFewExecution()
+    {
+        var rnd = new Random();
+        _defaultCircularCloudLayouter.PutNextRectangle(new Size(rnd.Next(5, 20), rnd.Next(2, 5)));
+        _defaultCircularCloudLayouter.PutNextRectangle(new Size(rnd.Next(5, 20), rnd.Next(2, 5)));
+        _defaultCircularCloudLayouter.PutNextRectangle(new Size(rnd.Next(5, 20), rnd.Next(2, 5)));
+
+        var lastRectangle = _defaultCircularCloudLayouter.PutNextRectangle(new Size(rnd.Next(5, 20), rnd.Next(2, 5)));
+
+        foreach (var rectangle in _defaultCircularCloudLayouter.Rectangles.Where(rectangle => rectangle != lastRectangle))
+        {
+            lastRectangle.Center.Should().NotBeEquivalentTo(rectangle.Center);
+        }
     }
 }
 
