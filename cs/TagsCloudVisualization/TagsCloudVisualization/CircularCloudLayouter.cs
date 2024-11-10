aaa9b901881a6a849a3ed80e06c5de227a1f43e0
Aleksandr Sazonov <alexxsaz96@gmail.com>
2024-11-10 23:40:29 +0500

    Added realization to GetFreeCenter method

--- cs/TagsCloudVisualization/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TagsCloudVisualization;
+﻿namespace TagsCloudVisualization;
 
 public class CircularCloudLayouter
 {
     private readonly Point _center;
+    private double _radius = 0;
+    private double _angle = 0;
+
     public LinkedList<Rectangle> Rectangles { get; } = new();
 
     public CircularCloudLayouter(Point center)
@@ -20,7 +17,10 @@ public class CircularCloudLayouter
     {
         Rectangle newRectangle;
         if (Rectangles.Count == 0)
+        {
             newRectangle = new Rectangle(size, _center);
+            _radius = Math.Sqrt(Math.Pow(newRectangle.Size.Width, 2) + Math.Pow(newRectangle.Size.Height, 2)) / 2;
+        }
         else
             newRectangle = new Rectangle(size, GetFreeCenter(size));
         
@@ -28,9 +28,11 @@ public class CircularCloudLayouter
         return Rectangles.Last?.Value ?? newRectangle;
     }
 
-    private static Point GetFreeCenter(Size size)
+    private Point GetFreeCenter(Size size)
     {
-        throw new NotImplementedException();
+        var newX = (int)(_center.X + _radius * Math.Cos(_angle));
+        var newY = (int)(_center.Y + _radius * Math.Sin(_angle));
+        return new Point(newX, newY);
     }
 }
 
