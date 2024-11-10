6cba9459931ccc268074b19bc54e590c14ac1d72
Aleksandr Sazonov <alexxsaz96@gmail.com>
2024-11-10 18:36:11 +0500

    Added constrains for width and height values

--- cs/TagsCloudVisualization/TagsCloudVisualization/Size.cs
+++ cs/TagsCloudVisualization/TagsCloudVisualization/Size.cs
@@ -8,7 +8,29 @@ namespace TagsCloudVisualization;
 
 public class Size(int width, int height)
 {
-    public int Width { get; } = width;
-    public int Height { get; } = height;
+    private int _width = width;
+    private int _height = height;
+
+    public int Width
+    {
+        get => _width;
+        private set
+        {
+            if (value is < 1 or > 100)
+                throw new ArgumentOutOfRangeException($"{nameof(Width)} can't be less than 1 or large than 100");
+            _width = value;
+        }
+    }
+
+    public int Height
+    {
+        get => _height;
+        private set
+        {
+            if (value is < 1 or > 100)
+                throw new ArgumentOutOfRangeException($"{nameof(Height)} can't be less than 1 or large than 100");
+            _height = value;
+        } 
+    }
 }
 
