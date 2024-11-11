f5e166b2b95667308513040bf25de1ca58133ebf
Aleksandr Sazonov <alexxsaz96@gmail.com>
2024-11-11 22:42:54 +0500

    Inited points generator class

---
+++ cs/TagsCloudVisualization/TagsCloudVisualization/PointsGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization;
+public class PointsGenerator
+{
+    public PointsGenerator(Point centerPoint)
+    {
+        var size = new Size(centerPoint);
+    }
+
+    public Point GetBestPoint()
+    {
+        return new Point(0, 0);
+    }
+}
---
+++ cs/TagsCloudVisualization/TagsCloudVisualization/PointsGeneratorShould.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using System.Drawing;
+
+namespace TagsCloudVisualization;
+
+[TestFixture]
+public class PointsGeneratorShould
+{
+    private PointsGenerator _pointsGenerator;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _pointsGenerator = new PointsGenerator();
+    }
+
+    [TestCase()]
+    public void GetBestPoint_ReturnPoint_AfterExecutingWith()
+    {
+        _pointsGenerator.GetBestPoint().Should().NotBeNull();
+    }
+}
