07281faac8880908561c3b9e971c549db178f21c
Alexxsaz <alexxsaz96@gmail.com>
2024-11-11 18:42:19 +0500

    Added methods to select place for new rectangles

--- cs/TagsCloudVisualization/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -5,6 +5,8 @@ public class CircularCloudLayouter
     private readonly Point _center;
     private double _radius = 0;
     private double _angle = 0;
+    private double _spiralStep = 10; // Шаг спирали
+    private double _spiralRotation = 10; // Угол вращения спирали на каждый шаг
 
     private LinkedList<Rectangle> _rectangles { get; } = new();
 
@@ -15,24 +17,69 @@ public class CircularCloudLayouter
 
     public Rectangle PutNextRectangle(Size size)
     {
-        Rectangle newRectangle;
         if (_rectangles.Count == 0)
         {
-            newRectangle = new Rectangle(size, _center);
-            _radius = Math.Sqrt(Math.Pow(newRectangle.Size.Width, 2) + Math.Pow(newRectangle.Size.Height, 2)) / 2;
+            _radius = GetRadiusFromSize(size);
+            _rectangles.AddLast(new Rectangle(size, _center));
+            return _rectangles.Last.Value;
         }
-        else
-            newRectangle = new Rectangle(size, GetFreeCenter(size));
+
+        var newRectangle = FindFreeSpaceOnSpiral(size);
         
         _rectangles.AddLast(newRectangle);
         return _rectangles.Last?.Value ?? newRectangle;
     }
 
-    private Point GetFreeCenter(Size size)
+    private Rectangle FindFreeSpaceOnSpiral(Size rectangleSize)
     {
-        var newX = (int)(_center.X + _radius * Math.Cos(_angle));
-        var newY = (int)(_center.Y + _radius * Math.Sin(_angle));
-        return new Point(newX, newY);
+        double angle = 0;
+        double offset = _radius;
+        double spiralLength = 0;
+
+        while (true)
+        {
+            var center = GetFreeCenter(rectangleSize, offset, spiralLength);
+            var rect = new Rectangle(rectangleSize, center);
+
+            if (!_rectangles.Any(r => r.IntersectsWith(rect)))
+            {
+                rect = MoveTowardsCenter(rect, rectangleSize);
+                _angle = angle;
+                _radius = offset;
+                return rect;
+            }
+
+            // 5. Поиск нового смещения
+            angle += _spiralRotation;
+            offset += _spiralStep;
+            spiralLength += _spiralStep;
+        }
+    }
+
+    private Rectangle MoveTowardsCenter(Rectangle rect, Size rectangleSize)
+    {
+        var distanceToCenter = Math.Sqrt(Math.Pow(rect.Center.X + rect.Size.Width / 2 - _center.X, 2) + Math.Pow(rect.Center.Y + rect.Size.Height / 2 - _center.Y, 2));
+
+        // 2. Расчет сдвига к центру
+        var shift = Math.Min(distanceToCenter / 2, rectangleSize.Width / 4);
+
+        // 3. Сдвиг прямоугольника к центру
+        return new Rectangle(rect.Size, new Point((rect.Center.X - shift), (rect.Center.Y - shift)));
+    }
+
+    private static double GetRadiusFromSize(Size size)
+    {
+        return Math.Sqrt(Math.Pow(size.Width, 2) + Math.Pow(size.Height, 2)) / 2;
+    }
+    private Point GetFreeCenter(Size size, double radius, double spiralLength)
+    {
+        var angleRad = _angle * Math.PI / 180;
+
+        // Вычисление координат точки на спирали
+        var x = _center.X + radius * Math.Cos(angleRad) + spiralLength * Math.Sin(angleRad);
+        var y = _center.Y + radius * Math.Sin(angleRad) - spiralLength * Math.Cos(angleRad);
+
+        return new Point(x, y);
     }
 }
 
