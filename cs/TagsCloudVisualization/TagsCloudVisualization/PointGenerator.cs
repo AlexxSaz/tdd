55e34baf1eaa84aded138ef17bab04e3348772fe
Alexxsaz <alexxsaz96@gmail.com>
2024-11-12 11:12:16 +0500

    Added implementation to GetNewPoint and tests

---
+++ cs/TagsCloudVisualization/TagsCloudVisualization/PointGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization;
+public class PointGenerator(Point centerPoint)
+{
+    private const double _angleStep = Math.PI / 180;
+    private const double _radiusStep = 0.1;
+    private double _radius;
+    private double _angle;
+    private readonly Size _centerSize = new Size(centerPoint);
+
+    public Point GetNewPoint()
+    {
+        var newX = (int)(_radius * Math.Cos(_angle));
+        var newY = (int)(_radius * Math.Sin(_angle));
+        var newPoint = new Point(newX, newY);
+        newPoint = Point.Add(newPoint, _centerSize);
+
+        _angle += _angleStep;
+        _radius += _radiusStep;
+
+        return newPoint;
+    }
+}
---
+++ cs/TagsCloudVisualization/TagsCloudVisualization/PointGeneratorShould.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using System.Drawing;
+
+namespace TagsCloudVisualization;
+
+[TestFixture]
+public class PointGeneratorShould
+{
+    private PointGenerator _defaultPointGenerator;
+    private Point _defaultCenter;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _defaultCenter = new Point(5, 2);
+        _defaultPointGenerator = new PointGenerator(_defaultCenter);
+    }
+
+    [Test]
+    public void GetNewPoint_ReturnCenter_AfterFirstExecution()
+    {
+        var point = _defaultPointGenerator.GetNewPoint();
+
+        point.Should().BeEquivalentTo(_defaultCenter);
+    }
+
+    [Test]
+    public void ReturnUniqPointEveryTime_AfterExecution()
+    {
+        const int count = 101;
+        const int stepForCheck = 10;
+        var pointsHashset = new HashSet<Point>();
+
+        for (var i = 0; i < count; i++)
+        {
+            var currPoint = _defaultPointGenerator.GetNewPoint();
+            if (i % stepForCheck == 0)
+                pointsHashset.Add(currPoint);
+        }
+
+        pointsHashset.Should().HaveCount(count / stepForCheck);
+    }
+}
