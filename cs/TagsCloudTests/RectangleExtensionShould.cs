0ac9619e6c1a3cf44f16c4bdc761355dabb10fb4
Aleksandr Sazonov <alexxsaz96@gmail.com>
2024-11-16 18:53:37 +0500

    Added extension for rectangle

---
+++ cs/TagsCloudTests/RectangleExtensionShould.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using FluentAssertions;
+using TagsCloudVisualization.Extensions;
+
+namespace TagsCloudTests;
+
+[TestFixture]
+public class RectangleExtensionShould
+{
+    [Test]
+    [Repeat(5)]
+    public void GetCentralPoint_ReturnExpectedCenter_AfterExecutionWithRandomRectangle()
+    {
+        var rectangleLocation = new Point(0, 0);
+        var random = new Random();
+        var rectangleSize = new Size(random.Next(1, 100), random.Next(1, 100));
+        var rectangle = new Rectangle(rectangleLocation, rectangleSize);
+        var expectedCentralPoint = new Point(rectangleLocation.X + rectangleSize.Width / 2,
+            rectangleLocation.Y - rectangleSize.Height / 2);
+
+        rectangle.GetCentralPoint().Should().Be(expectedCentralPoint);
+    }
+}
+
---
+++ cs/TagsCloudVisualization/Extensions/RectangleExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization.Extensions;
+
+public static class RectangleExtension
+{
+    public static Point GetCentralPoint(this Rectangle rectangle)
+    {
+        var centerPoint = rectangle.Location;
+        centerPoint.Offset(rectangle.Width / 2, -rectangle.Height / 2);
+        return centerPoint;
+    }
+}
+
