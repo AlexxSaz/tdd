9047c0ef4f2c7a44151ff5a83f8f9ae93f411963
Alexxsaz <alexxsaz96@gmail.com>
2024-11-16 09:46:03 +0500

    TagCloud added

---
+++ cs/TagsCloudTests/TagCloudShould.cs
@@ -0,0 +1,65 @@
+﻿using System.Drawing;
+using FluentAssertions;
+using TagsCloudVisualization;
+
+namespace TagsCloudTests;
+
+[TestFixture]
+public class TagCloudShould
+{
+    [TestCase(0, 0)]
+    public void PlacedInSelectedCenter_AfterCreation(int x, int y)
+    {
+        var center = new Point(x, y);
+        var firstRectangleSize = new Size(2, 2);
+        var circularCloudLayouter = new CircularCloudLayouter(center);
+        var tagCloud = new TagCloud(circularCloudLayouter);
+
+        tagCloud.PutNextRectangle(firstRectangleSize);
+
+        tagCloud.Rectangles[0].X.Should().Be(x);
+        tagCloud.Rectangles[0].Y.Should().Be(y);
+    }
+
+    [Test]
+    public void HaveNoRectangles_AfterCreation()
+    {
+        var center = new Point(0, 0);
+        var layouter = new CircularCloudLayouter(center);
+        var tagCloud = new TagCloud(layouter);
+
+        tagCloud.Rectangles.Should().HaveCount(0);
+    }
+
+    [TestCase(5, ExpectedResult = 5)]
+    public int HaveExpectedWidth_AfterAddedFirstRectangleWith(int width)
+    {
+        var newSize = new Size(width, 2);
+        var center = new Point(0, 0);
+        var layouter = new CircularCloudLayouter(center);
+        var tagCloud = new TagCloud(layouter);
+
+        tagCloud.PutNextRectangle(newSize);
+
+        return tagCloud.Width;
+    }
+
+    [TestCase(5, ExpectedResult = 5)]
+    public int HaveExpectedHeight_AfterAddedFirstRectangleWith(int height)
+    {
+        var newSize = new Size(2, height);
+        var center = new Point(0, 0);
+        var layouter = new CircularCloudLayouter(center);
+        var tagCloud = new TagCloud(layouter);
+
+        tagCloud.PutNextRectangle(newSize);
+
+        return tagCloud.Height;
+    }
+
+    [Test]
+    public void HaveCircularForm_WhenLayoutIsCircular()
+    {
+
+    }
+}
---
+++ cs/TagsCloudVisualization/TagCloud.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using TagsCloudVisualization.Interfaces;
+
+namespace TagsCloudVisualization;
+
+public class TagCloud(ICloudLayouter? layouter)
+{
+    private readonly ICloudLayouter _layouter = layouter ?? new CircularCloudLayouter(new Point(0, 0));
+    public List<Rectangle> Rectangles { get; } = [];
+
+    public void PutNextRectangle(Size size)
+    {
+        var nextRectangle = _layouter.PutNextRectangle(size);
+        Rectangles.Add(nextRectangle);
+    }
+
+    public int Width
+    {
+        get
+        {
+            if (Rectangles.Count == 0)
+                return 0;
+
+            return Rectangles.Max(rect => rect.Right) -
+                   Rectangles.Min(rect => rect.Left);
+        }
+    }
+
+    public int Height
+    {
+        get
+        {
+            if (Rectangles.Count == 0)
+                return 0;
+
+            return Rectangles.Max(rect => rect.Bottom) -
+                   Rectangles.Min(rect => rect.Top);
+        }
+    }
+}
