3ad22ea1b121a9a084a2dfc1d6f58a6d2d36af9b
Aleksandr Sazonov <alexxsaz96@gmail.com>
2024-11-16 18:54:45 +0500

    Changed point extensions

---
+++ cs/TagsCloudTests/PointExtensionShould.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+using FluentAssertions;
+using TagsCloudVisualization.Extensions;
+
+namespace TagsCloudTests;
+
+[TestFixture]
+public class PointExtensionShould
+{
+    [TestCase(0, 0, 1, 2)]
+    [TestCase(0, 0, 1, 0)]
+    [TestCase(3, 5, 0, 5)]
+    public void PointShiftTo_ReturnedMovedPoint_WhenSet(int pointX, int pointY, int shiftX, int shiftY)
+    {
+        var point = new Point(pointX, pointY);
+        var movementDirection = new Size(shiftX, shiftY);
+
+        var movedPoint = Point.Add(point, movementDirection);
+
+        MoveTo_CheckShift(point, movementDirection, movedPoint);
+    }
+
+    [Test]
+    public void MoveTo_ReturnedNotMovedPoint_WhenSetZeroDirection()
+    {
+        var point = new Point(5, 6);
+
+        MoveTo_CheckShift(point, new Size(0, 0), point);
+    }
+
+    public void MoveTo_CheckShift(Point point, Size movementDirection, Point expectedPoint)
+    {
+        var movedPoint = point.MoveTo(movementDirection);
+
+        movedPoint.Should().BeEquivalentTo(expectedPoint);
+    }
+}
+
---
+++ cs/TagsCloudVisualization/Extensions/PointExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization.Extensions;
+
+public static class PointExtension
+{
+    public static Point MoveTo(this Point point, Size direction) =>
+        Point.Add(point, direction);
+
+    public static double GetDistanceTo(this Point point1, Point point2) =>
+        Math.Sqrt(Math.Pow(point1.X - point2.X, 2) + Math.Pow(point1.Y - point2.Y, 2));
+}
+
