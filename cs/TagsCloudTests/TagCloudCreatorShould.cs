02a9b7453482a6f49da5b9f65c8b24175fb5ff28
Aleksandr Sazonov <alexxsaz96@gmail.com>
2024-11-17 10:54:45 +0500

    Added class that creates tag cloud example

---
+++ cs/TagsCloudTests/TagCloudCreatorShould.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+using FluentAssertions;
+using TagsCloudVisualization;
+
+namespace TagsCloudTests;
+
+[TestFixture]
+[Parallelizable(ParallelScope.All)]
+public class TagCloudCreatorShould
+{
+    private Point _defaultCenter = new(0, 0);
+    private Random _random = new();
+
+    [Test]
+    [Repeat(5)]
+    public void Create_ReturnTagCloudEquivalentToConstructor_AfterExecution()
+    {
+        var rectangleSizes = Enumerable.Range(_random.Next(2, 10), _random.Next(20, 100)).
+            Select(width => new Size(width, width / 2)).Reverse();
+        var currCloudLayouter = new CircularCloudLayouter(_defaultCenter);
+        var expectedCloudLayouter = new CircularCloudLayouter(_defaultCenter);
+        var defaultTagCloud = new TagCloud(currCloudLayouter);
+        foreach (var rectangleSize in rectangleSizes)
+            defaultTagCloud.AddNextRectangleWith(rectangleSize);
+
+        var expectedTagCloud = TagCloudCreator.Create(rectangleSizes, expectedCloudLayouter);
+
+        defaultTagCloud.Should().BeEquivalentTo(expectedTagCloud);
+    }
+}
---
+++ cs/TagsCloudVisualization/TagCloudCreator.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using TagsCloudVisualization.Interfaces;
+
+namespace TagsCloudVisualization;
+
+public class TagCloudCreator
+{
+    public static TagCloud Create(IEnumerable<Size> rectangleSizes, ICloudLayouter cloudLayouter)
+    {
+        var newTagCloud = new TagCloud(cloudLayouter);
+
+        foreach (var rectangleSize in rectangleSizes)
+            newTagCloud.AddNextRectangleWith(rectangleSize);
+
+        return newTagCloud;
+    }
+}
